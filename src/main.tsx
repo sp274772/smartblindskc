7de4dc8f8f9ff7dda957be790a8b5ab4ecbb78ee
sp274772 <sp274772@gmail.com>
2026-04-08 22:15:59 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import './index.css';
 document.title = "SmartBlindsKC";
 console.log("Title set to SmartBlindsKC");
 
+// Keep setting the title for a few seconds to override any platform defaults
+const titleInterval = setInterval(() => {
+  if (document.title !== "SmartBlindsKC") {
+    document.title = "SmartBlindsKC";
+  }
+}, 100);
+
+setTimeout(() => clearInterval(titleInterval), 5000);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
